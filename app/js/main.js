b941c2e67cd3d4e58584d2180bb94fe2a4b885b0
Sven Habex <sven.habex@hotmail.com>
2017-02-08 08:14:28 +0100

    delete task function complete

--- app/js/main.js
+++ app/js/main.js
@@ -21,24 +21,28 @@ var Tasks = {
         this.addTask();
       }
     });
-    this.taskList.addEventListener("click", this.deleteTask.bind(this));
+    this.taskList.addEventListener("click", (e) => {
+      if(e.target && e.target.matches("span") || e.target.matches("svg") || e.target.matches("path")){
+        this.deleteTask(e);
+      }
+    });
   },
 
-  addTask: function(){
-    if(this.addInput.value === "") return;
-    this.tasks.push(this.addInput.value);
+  addTask: function(value){
+    if(this.addInput.value === "" && !value) return;
+    this.tasks.push(value || this.addInput.value);
     this.render();
     this.addInput.value = "";
   },
 
   deleteTask: function(e){
-    if(e.target && e.target.matches("span") || e.target.matches("svg") || e.target.matches("path")){
-      console.log("kruiske");
-    }
+    let remove = e.target.closest(".task").dataset.id;
+    this.tasks.splice(remove, 1);
+    this.render();
   },
 
   render: function(){
-    this.taskList.innerHTML = this.tasks.map((task) => `<li class="task">${task}<span><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    this.taskList.innerHTML = this.tasks.map((task, i) => `<li class="task" data-id="${i}">${task}<span><svg style="width:24px;height:24px" viewBox="0 0 24 24">
     <path class="x" fill="#000000" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
     </svg></span></li>`).join("");
   }
